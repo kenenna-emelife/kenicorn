a72ea13d15abb813ba312b4322f5bbd94b720bcb
kenenna-emelife <kenennaemelife@gmail.com>
2022-08-25 03:03:40 +0200

    i made a branch

--- index.js
+++ index.js
@@ -11,6 +11,5 @@ for( i = 0; i < slides.length; i++){
 slideIndex++;
 if (slideIndex > slides.length){slideIndex= 1}
 slides[slideIndex-1].style.display ="block";
-
 setTimeout(showSlides, 2000);
  }
